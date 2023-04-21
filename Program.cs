9957dfedff612c14793a2e40548988a19cbff1df
Minseon Song <songminseonk@gmail.com>
2023-04-21 14:10:28 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Say Something: ");
+string? input = Console.ReadLine();
+
+if(input == null) {
+    Console.WriteLine("You didn't say anything");
+}
+else {
+    Console.WriteLine($"You said: {input}");
+}
+
